74246e40773d407a007a45b128f895d4c1b6a1f2
Ioan Calin <iborco@gmail.com>
2021-06-01 18:58:58 +0300

    create dummy viewer app

---
+++ src/tutcatalogpy/viewer/main.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+
+from PySide2.QtWidgets import QApplication
+
+from tutcatalogpy.common.settings import setup_settings
+from tutcatalogpy.common.widgets.application import Application
+from tutcatalogpy.viewer.widgets.main_window import MainWindow
+
+log = logging.getLogger(__name__)
+log.info('Launching app.')
+
+
+def run():
+    setup_settings('tutcatalogpy2-viewer')
+
+    # can't extend QApplication because of a bug in Qt
+    # that will result in a SIGSEGV when the garbage
+    # collector runs at exit...
+    Application.set_title(MainWindow.WINDOW_TITLE)
+    app = QApplication(sys.argv)
+    Application.set_icon(app, MainWindow.WINDOW_ICON_FILE)
+
+    window = MainWindow()
+    window.read_settings()
+    window.show()
+
+    success = app.exec_()
+    sys.exit(success)
+
+
+if __name__ == '__main__':
+    run()
--- src/tutcatalogpy/viewer/widgets/main_window.py
+++ src/tutcatalogpy/viewer/widgets/main_window.py
@@ -3,5 +3,5 @@ from tutcatalogpy.common.widgets.main_window import CommonMainWindow
 
 
 class MainWindow(CommonMainWindow):
-    WINDOW_TITLE = 'TutCatalogPy2 Catalog'
-    WINDOW_ICON_FILE = relative_path(__file__, '../../resources/icons/catalog.png')
+    WINDOW_TITLE = 'TutCatalogPy2 Viewer'
+    WINDOW_ICON_FILE = relative_path(__file__, '../../resources/icons/viewer.png')
