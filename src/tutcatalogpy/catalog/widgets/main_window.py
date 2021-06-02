33c4b99ca3e05c28cf90a3f4f4dd4f87ab52b395
Ioan Calin <iborco@gmail.com>
2021-06-02 16:16:12 +0300

    copy recent files from old project

--- src/tutcatalogpy/catalog/widgets/main_window.py
+++ src/tutcatalogpy/catalog/widgets/main_window.py
@@ -1,23 +1,40 @@
+import logging
 from typing import Final
 
-from PySide2.QtWidgets import QFrame, QLabel
+from PySide2.QtGui import QKeySequence
+from PySide2.QtWidgets import QAction, QFrame, QLabel, QMenu, QMenuBar
 
 from tutcatalogpy.common.files import relative_path
+from tutcatalogpy.common.recent_files import RecentFiles
 from tutcatalogpy.common.widgets.logging_dock import LoggingDock
 from tutcatalogpy.common.widgets.main_window import CommonMainWindow
+
+log = logging.getLogger(__name__)
+log.info('Launching app.')
 
 
 class MainWindow(CommonMainWindow):
     WINDOW_TITLE: Final[str] = 'TutCatalogPy2 Catalog'
     WINDOW_ICON_FILE: Final[str] = relative_path(__file__, '../../resources/icons/catalog.png')
 
+    FILE_MENU: Final[str] = 'File'
+    FILE_OPEN_MENU: Final[str] = 'Open...'
+    FILE_OPEN_SHORTCUT: Final[QKeySequence] = QKeySequence.Open
+    FILE_OPEN_TIP: Final[str] = 'Open config ...'
+
+    FILE_QUIT_MENU: Final[str] = 'Quit'
+    FILE_QUIT_SHORTCUT: Final[QKeySequence] = QKeySequence.Quit
+
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.setWindowTitle(self.WINDOW_TITLE)
 
+        self.__recent_files = RecentFiles(self)
+
         self._setup_statusbar()
         self._setup_docks()
+        self.__setup_menus()
         self._setup_toolbars()
 
     def _setup_statusbar(self) -> None:
@@ -39,6 +56,46 @@ class MainWindow(CommonMainWindow):
         ]
 
         super()._setup_docks()
+
+    def __setup_menus(self) -> None:
+        self.__menubar = QMenuBar()
+        self.setMenuBar(self.__menubar)
+
+        # self.__menubar.setNativeMenuBar(False)
+
+        # File Menu
+        menu = QMenu(self.FILE_MENU, self)
+
+        action = QAction(self.FILE_OPEN_MENU, self)
+        action.setShortcut(self.FILE_OPEN_SHORTCUT)
+        action.setStatusTip(self.FILE_OPEN_TIP)
+        action.triggered.connect(self.__open_config)
+        menu.addAction(action)
+
+        self.__recent_files.triggered.connect(self.load_config)
+        menu.addMenu(self.__recent_files.menu)
+
+        action = QAction(self.FILE_QUIT_MENU, self)
+        action.setShortcut(self.FILE_QUIT_SHORTCUT)
+        action.setMenuRole(QAction.QuitRole)
+        action.triggered.connect(self.close)
+        menu.addAction(action)
+
+        self.__menubar.addMenu(menu)
+
+        # Views Menu
+        menu = QMenu('Docks', self)
+
+        for dock in self._docks:
+            menu.addAction(dock.toggleViewAction())
+
+        self.__menubar.addMenu(menu)
+
+    def load_config(self, file_name: str) -> None:
+        log.info('Loading config file: %s', file_name)
+
+    def __open_config(self) -> None:
+        log.info('Open config file trigerred')
 
 
 if __name__ == '__main__':
---
+++ src/tutcatalogpy/common/recent_files.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from typing import Dict, Final, Union
+
+from PySide2.QtCore import QObject, QSettings, Signal
+from PySide2.QtWidgets import QAction, QMenu, QWidget
+
+
+class RecentFiles(QObject):
+    SETTINGS_ARRAY: Final[str] = 'recent_files'
+    FILE_SETTINGS_KEY: Final[str] = 'file'
+
+    CLEAR_ACTION_TEXT: Final[str] = 'Clear Recently Opened'
+
+    triggered = Signal(str)
+
+    def __init__(self, parent: QWidget = None) -> None:
+        super().__init__(parent)
+        self.__actions: Dict[str, QAction] = {}
+
+        self.__clearAction: Final[QAction] = QAction(self.CLEAR_ACTION_TEXT, self)
+        self.__clearAction.triggered.connect(self.clear)
+
+        self.__menu: Final[QMenu] = QMenu('Open Recent')
+        self.__menu.addSeparator()
+        self.__menu.addAction(self.__clearAction)
+
+        self.__menu.setEnabled(False)
+
+    @property
+    def menu(self) -> QMenu:
+        return self.__menu
+
+    def most_recent_action(self) -> Union[QAction, None]:
+        action = self.__menu.actions()[0]
+        if action.isSeparator() is False:
+            return action
+        return None
+
+    def mostRecentFile(self) -> Union[str, None]:
+        action = self.most_recent_action()
+        if action is not None:
+            return action.data()
+        return None
+
+    def load_settings(self, settings: QSettings):
+        size = settings.beginReadArray(self.SETTINGS_ARRAY)
+        for index in range(size - 1, -1, -1):
+            settings.setArrayIndex(index)
+            self.add_file(settings.value(self.FILE_SETTINGS_KEY))
+        settings.endArray()
+
+    def save_settings(self, settings: QSettings):
+        settings.beginWriteArray(self.SETTINGS_ARRAY)
+        actions = self.__menu.actions()[:-2]
+        for index in range(len(actions)):
+            settings.setArrayIndex(index)
+            action: QAction = actions[index]
+            settings.setValue(self.FILE_SETTINGS_KEY, action.data())
+        settings.endArray()
+
+    def clear(self):
+        for _, action in self.__actions.items():
+            self.__menu.removeAction(action)
+        self.__actions.clear()
+        self.__menu.setEnabled(False)
+
+    def add_file(self, fileName: str, toTop: bool = True):
+        action = self.__actions.pop(fileName, None)
+        if action is not None:
+            self.__menu.removeAction(action)
+
+        action = QAction(self)
+        action.setText(Path(fileName).name)
+        action.setData(fileName)
+        action.setStatusTip(f'Open config {fileName}')
+        action.triggered.connect(lambda: self.triggered.emit(fileName))
+        self.__actions[fileName] = action
+        self.__menu.insertAction(self.__menu.actions()[0], action)
+        self.__menu.setEnabled(True)
