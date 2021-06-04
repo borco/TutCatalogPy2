f703510ddea3a1a4c78fece99c1169766fc29ae2
Ioan Calin <iborco@gmail.com>
2021-06-04 13:17:45 +0300

    log settings location

--- src/tutcatalogpy/common/settings.py
+++ src/tutcatalogpy/common/settings.py
@@ -1,4 +1,9 @@
+import logging
+
 from PySide2.QtCore import QCoreApplication, QSettings
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
 
 
 def setup_settings(name: str) -> None:
@@ -6,3 +11,6 @@ def setup_settings(name: str) -> None:
     QCoreApplication.setOrganizationDomain('borco.ro')
     QCoreApplication.setApplicationName(name)
     QSettings.setDefaultFormat(QSettings.IniFormat)
+    settings = QSettings()
+    log.info('Using config file: %s', settings.fileName())
+    del settings
