76670640584d4ef3dee40ec90c2993c34aa52758
Ioan Calin <iborco@gmail.com>
2021-06-09 13:49:33 +0300

    test that folder id_ is kept when folder is renamed; added type annotations to session

--- tests/catalog/test_scan_worker.py
+++ tests/catalog/test_scan_worker.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Final, List
 
 from pytest import fixture, mark
+from sqlalchemy.orm.session import Session
 
 from tutcatalogpy.catalog.scan_config import ScanConfig
 from tutcatalogpy.catalog.scan_worker import ScanWorker
@@ -15,13 +16,13 @@ import tutcatalogpy.common.logging_config  # noqa: F401
 @fixture
 # def session(tmp_path):
 #     dal.connect(f'sqlite:///{tmp_path}/test.db')
-def session():
+def session() -> Session:
     dal.connect('sqlite:///:memory:')
     yield dal.Session()
     dal.disconnect()
 
 
-def test_scan_disks_for_offline_disks(tmp_path, session):
+def test_scan_disks_for_offline_disks(tmp_path: Path, session: Session):
     DISK_PARENT: Final[str] = str(tmp_path)
     DISK_NAME: Final[str] = 'foo'
 
@@ -34,7 +35,7 @@ def test_scan_disks_for_offline_disks(tmp_path, session):
     assert session.query(Disk).one().online is False
 
 
-def test_scan_disks_for_online_disks(tmp_path, session):
+def test_scan_disks_for_online_disks(tmp_path: Path, session: Session):
     DISK_PARENT: Final[str] = str(tmp_path)
     DISK_NAME: Final[str] = 'foo'
     disk_path: Path = tmp_path / DISK_NAME
@@ -50,7 +51,7 @@ def test_scan_disks_for_online_disks(tmp_path, session):
     assert session.query(Disk).one().online is True
 
 
-def test_scan_disks_updates_online(tmp_path, session):
+def test_scan_disks_updates_online(tmp_path: Path, session: Session):
     DISK_PARENT: Final[str] = str(tmp_path)
     DISK_NAME: Final[str] = 'foo'
     disk_path: Path = tmp_path / DISK_NAME
@@ -82,7 +83,7 @@ def test_scan_disks_updates_online(tmp_path, session):
         ('folder1', 'folder2'),
     ]
 )
-def test_scan_folders_for_new_db(tmp_path, session, folder_names: List[str]):
+def test_scan_folders_for_new_db(tmp_path: Path, session: Session, folder_names: List[str]):
     DISK_PARENT: Final[str] = str(tmp_path)
     DISK_NAME: Final[str] = 'disk1'
     disk_path: Path = tmp_path / DISK_NAME
@@ -111,7 +112,7 @@ def test_scan_folders_for_new_db(tmp_path, session, folder_names: List[str]):
         (['folder1', 'folder2'], ['folder1', 'folder2'], []),
     ]
 )
-def test_scan_folders_removes_deleted_folders(tmp_path, session, folder_names: List[str], deleted_names: List[str], remaining_names: List[str]):
+def test_scan_folders_removes_deleted_folders(tmp_path: Path, session: Session, folder_names: List[str], deleted_names: List[str], remaining_names: List[str]):
     DISK_PARENT: Final[str] = str(tmp_path)
     DISK_NAME: Final[str] = 'disk1'
     disk_path: Path = tmp_path / DISK_NAME
@@ -133,3 +134,34 @@ def test_scan_folders_removes_deleted_folders(tmp_path, session, folder_names: L
     worker.scan(mode)
 
     assert {x for x, in session.query(Folder.folder_name).all()} == set(remaining_names)
+
+
+def test_scan_folders_recognizes_moved_folders(tmp_path: Path, session: Session):
+    DISK_PARENT: Final[str] = str(tmp_path)
+    DISK_NAME: Final[str] = 'disk1'
+    FOLDER_NAME1: Final[str] = 'folder1'
+    FOLDER_NAME2: Final[str] = 'folder2'
+    FOLDER_NAME3: Final[str] = 'folder3'
+
+    disk_path: Path = tmp_path / DISK_NAME
+
+    session.add(Disk(disk_parent=DISK_PARENT, disk_name=DISK_NAME, index_=0, online=True))
+    session.commit()
+
+    disk_path.mkdir()
+    (disk_path / FOLDER_NAME1).mkdir(parents=True)
+    (disk_path / FOLDER_NAME2).mkdir(parents=True)
+
+    mode = ScanConfig.Mode.EXTENDED
+    worker = ScanWorker()
+    worker.scan(mode)
+
+    folder1_id = session.query(Folder).filter(Folder.folder_name == FOLDER_NAME1).one().id_
+
+    (disk_path / FOLDER_NAME1).rename(disk_path / FOLDER_NAME3)
+
+    worker.scan(mode)
+
+    folder3_id = session.query(Folder).filter(Folder.folder_name == FOLDER_NAME3).one().id_
+
+    assert folder1_id == folder3_id
