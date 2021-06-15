c824d834f28f4a949cfbbb28dd36ab4437626924
Ioan Calin <iborco@gmail.com>
2021-06-15 18:37:09 +0300

    center pixmap in label

--- src/tutcatalogpy/common/widgets/aspectratiopixmaplabel.py
+++ src/tutcatalogpy/common/widgets/aspectratiopixmaplabel.py
@@ -17,10 +17,11 @@ class AspectRatioPixmapLabel(QLabel):
 
     def setPixmap(self, pixmap: Optional[QPixmap]) -> None:
         self.__pixmap = pixmap
-        super().setPixmap(self.__scaled_pixmap())
+        super().setPixmap(self.__pixmap)
+        self.resizeEvent(None)
 
     def heightForWidth(self, width: int) -> int:
-        return self.height() if self.__pixmap is None else int(self.__pixmap.height() * width / self.__pixmap.width())
+        return self.height() if self.__pixmap is None else self.__pixmap.height() * width // self.__pixmap.width()
 
     def sizeHint(self) -> QSize:
         if self.__pixmap is None:
@@ -30,7 +31,7 @@ class AspectRatioPixmapLabel(QLabel):
         height = self.heightForWidth(width)
         if height > self.height():
             height = self.height()
-            width = int(self.__pixmap.width() * height / self.__pixmap.height())
+            width = self.__pixmap.width() * height // self.__pixmap.height()
         return QSize(width, height)
 
     def __scaled_pixmap(self) -> QPixmap:
@@ -41,7 +42,17 @@ class AspectRatioPixmapLabel(QLabel):
 
     def resizeEvent(self, event: QResizeEvent) -> None:
         if self.__pixmap is not None:
-            super().setPixmap(self.__scaled_pixmap())
+            pixmap = self.__scaled_pixmap()
+            width = self.width()
+            pixmap_width = pixmap.width()
+
+            if width > pixmap_width:
+                m = (width - pixmap_width) // 2
+                self.setContentsMargins(m, 0, width - m - pixmap_width, 0)
+            else:
+                self.setContentsMargins(0, 0, 0, 0)
+
+            super().setPixmap(pixmap)
 
 
 if __name__ == '__main__':
