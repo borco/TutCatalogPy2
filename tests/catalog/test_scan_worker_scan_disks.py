b8972dcd2349d10846c18b96c4050803eee48224
Ioan Calin <iborco@gmail.com>
2021-06-19 13:42:27 +0300

    removed unused imports

--- tests/catalog/test_scan_worker_scan_disks.py
+++ tests/catalog/test_scan_worker_scan_disks.py
@@ -1,15 +1,14 @@
 import shutil
 from pathlib import Path
-from typing import Final, List
+from typing import Final
 
-from pytest import fixture, mark
+from pytest import fixture
 from sqlalchemy.orm.session import Session
 
 from tutcatalogpy.catalog.scan_config import ScanConfig
 from tutcatalogpy.catalog.scan_worker import ScanWorker
 from tutcatalogpy.catalog.db.dal import dal
 from tutcatalogpy.catalog.db.disk import Disk
-from tutcatalogpy.catalog.db.folder import Folder
 import tutcatalogpy.common.logging_config  # noqa: F401
 
 
