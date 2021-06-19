2b5a0dd861b95a0c584297660988f203eaf1c1a1
Ioan Calin <iborco@gmail.com>
2021-06-19 16:02:23 +0300

    refactor

--- src/tutcatalogpy/catalog/db/tutorial_learning_path.py
+++ src/tutcatalogpy/catalog/db/tutorial_learning_path.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy.schema import Column, ForeignKey, UniqueConstraint
-from sqlalchemy.sql.schema import PrimaryKeyConstraint
+from sqlalchemy.schema import Column, ForeignKey
 from sqlalchemy.sql.sqltypes import Integer
 
 from tutcatalogpy.catalog.db.base import Base
@@ -10,18 +9,12 @@ class TutorialLearningPath(Base):
 
     __tablename__ = 'tutorial_learning_path'
 
-    tutorial_id = Column(Integer, ForeignKey('tutorial.id'))
-    learning_path_id = Column(Integer, ForeignKey('learning_path.id'))
-
+    tutorial_id = Column(Integer, ForeignKey('tutorial.id'), primary_key=True)
+    learning_path_id = Column(Integer, ForeignKey('learning_path.id'), primary_key=True)
     index = Column(Integer)  # index in learning path
 
     tutorial = relationship('Tutorial', backref='learning_paths')
     learning_path = relationship('LearningPath', backref='tutorials')
-
-    __table_args__ = (
-        PrimaryKeyConstraint('tutorial_id', 'learning_path_id'),
-        UniqueConstraint('learning_path_id', 'index'),
-    )
 
 
 if __name__ == '__main__':
