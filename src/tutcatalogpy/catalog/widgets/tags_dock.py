50a620dc312d6afaffbb0a74e736789baee65867
Ioan Calin <iborco@gmail.com>
2021-06-25 13:20:30 +0300

    added option to clear search flags

--- src/tutcatalogpy/catalog/widgets/tags_dock.py
+++ src/tutcatalogpy/catalog/widgets/tags_dock.py
@@ -1,8 +1,10 @@
 import logging
-from typing import Final
+from typing import Final, Optional
 
-from PySide2.QtWidgets import QTreeView
+from PySide2.QtGui import QIcon
+from PySide2.QtWidgets import QAction, QTreeView
 
+from tutcatalogpy.catalog.models.tags_model import TagsModel
 from tutcatalogpy.common.files import relative_path
 from tutcatalogpy.common.widgets.dock_widget import DockWidget
 
@@ -15,6 +17,9 @@ class TagsDock(DockWidget):
 
     DOCK_TITLE: Final[str] = 'Tags'
     DOCK_OBJECT_NAME: Final[str] = 'tags_dock'
+
+    CLEAR_ICON: Final[str] = relative_path(__file__, '../../resources/icons/clear.svg')
+    CLEAR_TIP: Final[str] = 'Clear tags used in search'
 
     _dock_icon: Final[str] = relative_path(__file__, '../../resources/icons/tags.svg')
     _dock_status_tip: Final[str] = 'Toggle tags dock'
@@ -38,10 +43,23 @@ class TagsDock(DockWidget):
         self.__tags_view.setHeaderHidden(True)
 
     def __setup_actions(self) -> None:
-        self._setup_dock_toolbar()
+        self.__clear = QAction()
+        self.__clear.setIcon(QIcon(self.CLEAR_ICON))
+        self.__clear.setStatusTip(self.CLEAR_TIP)
+        self.__clear.triggered.connect(self.__on_clear_triggered)
 
-    def set_model(self, model) -> None:
+        self._setup_dock_toolbar([
+            self.__clear
+        ])
+
+    def set_model(self, model: TagsModel) -> None:
         self.__tags_view.setModel(model)
+
+    def __on_clear_triggered(self) -> None:
+        model: Optional[TagsModel] = self.__tags_view.model()
+        if model is not None:
+            model.clear_search_flags()
+            self.__tags_view.expandAll()
 
 
 if __name__ == '__main__':
