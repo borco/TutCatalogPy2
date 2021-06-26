7e6da7ab423d56c8ceb48c3c44600f5b46d5b890
Ioan Calin <iborco@gmail.com>
2021-06-26 12:32:06 +0300

    derive PathView from ElidedLabel

--- src/tutcatalogpy/common/widgets/path_view.py
+++ src/tutcatalogpy/common/widgets/path_view.py
@@ -1,28 +1,31 @@
 from pathlib import Path
 from typing import Optional
 
-from PySide2.QtCore import Qt, QUrl
+from PySide2.QtCore import QUrl
 from PySide2.QtGui import QDesktopServices
-from PySide2.QtWidgets import QLabel
+
+from tutcatalogpy.common.widgets.elided_label import ElidedLabel
 
 
-class PathView(QLabel):
+class PathView(ElidedLabel):
     __path: Path = Path()
     __text: Optional[str] = None
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.setWordWrap(True)
-        self.setTextInteractionFlags(Qt.TextBrowserInteraction)
-        self.linkActivated.connect(self.__on_link_activated)
+        self.setInteractive(True)
+        self.triggered.connect(self.__on_triggered)
 
     def set_path(self, path: Path, text: Optional[str] = None) -> None:
         self.__path = path
         self.__text = text if text is not None else str(path)
-        self.setText(f'<a href="{self.__path}">{self.__text}</a>')
+        self.setText(self.__text)
+        self.setData(str(self.__path))
+        self.setInteractive(path.exists())
+        self.setStatusTip(f'Open in external file browser: {self.__path}')
 
-    def __on_link_activated(self, text: str) -> None:
+    def __on_triggered(self, text: str) -> None:
         if self.__path.exists():
             QDesktopServices.openUrl(QUrl(f'file://{self.__path}', QUrl.TolerantMode))
 
