ac331f33c1eab5228bd86dfb01a8cb4b2fdd3335
Ioan Calin Borcoman <iborco@gmail.com>
2021-09-16 12:47:05 +0300

    added chromedriver basic support

--- src/tutcatalogpy/scrapper/README.md
+++ src/tutcatalogpy/scrapper/README.md
@@ -16,13 +16,24 @@ Uses:
 https://github.com/mozilla/geckodriver/releases/latest
 
 ```sh
+# install firefox
+sudo apt install firefox-esr
+
+# install geckodriver
 ./get_linux_geckodriver.fish
 ```
 
 #### Chromedriver (Chrome)
 
-https://sites.google.com/chromium.org/driver/downloads
+* install chrome from Google
+* download chromedriver from https://sites.google.com/chromium.org/driver/downloads
 
 ```sh
+# install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+rm ./google-chrome-stable_current_amd64.deb
+
+# install chromedriver
 ./get_linux_chromedriver.fish
 ```
--- src/tutcatalogpy/scrapper/main.py
+++ src/tutcatalogpy/scrapper/main.py
@@ -1,9 +1,10 @@
-import urllib.parse
 import sys
+import sys
+import time
+import urllib.parse
 
 import click
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 import tutcatalogpy.scrapper.udemy as udemy
 from tutcatalogpy.common.files import relative_path
@@ -11,17 +12,28 @@ from tutcatalogpy.common.files import relative_path
 
 def make_driver(driver_name, show_driver):
     if driver_name == 'gecko':
-        binary_path = relative_path(__file__, 'drivers/geckodriver')
+        executable_path = relative_path(__file__, 'drivers/geckodriver')
         profile_path = relative_path(__file__, 'profiles/gecko.Default')
-        options = FirefoxOptions()
+        options = webdriver.FirefoxOptions()
         options.add_argument('-profile')
         options.add_argument(profile_path)
         driver = webdriver.Firefox(
-            executable_path=binary_path,
+            executable_path=executable_path,
             options=options,
             # hard-code the Marionette port so geckodriver can connect
             service_args=['--marionette-port', '2828'],
         )
+    elif driver_name == 'chrome':
+        executable_path = relative_path(__file__, 'drivers/chromedriver')
+        profile_path = relative_path(__file__, 'profiles/chrome.Default')
+        options = webdriver.ChromeOptions()
+        # options.add_argument('--no-headless')
+        options.add_argument(f'user-data-dir={profile_path}')  # Path to your chrome profile
+        driver = webdriver.Chrome(
+            executable_path=executable_path,
+            options=options,
+        )
+        driver.maximize_window()
     else:
         driver = None
     return driver
@@ -54,6 +66,8 @@ def run(url, driver_name, show_driver, images, verbose):
         print(f'Could not create driver: {driver_name}')
         sys.exit(1)
 
+    driver.get(url)
+
     for scrapper_class in scrappers:
         scrapper = scrapper_class()
         if scrapper.scrap(parsed_url.netloc, url, driver, images, verbose):
@@ -64,6 +78,8 @@ def run(url, driver_name, show_driver, images, verbose):
     if not scrapped:
         print(f"Don't know how to scrap: {url}")
         sys.exit(1)
+
+    time.sleep(5)
 
 
 if __name__ == '__main__':
