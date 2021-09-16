e5f46e953ad5be1bc98d0a4f76cb54f0f3114077
Ioan Calin Borcoman <iborco@gmail.com>
2021-09-16 11:18:35 +0300

    added some options using click

--- src/tutcatalogpy/scrapper/main.py
+++ src/tutcatalogpy/scrapper/main.py
@@ -1,5 +1,19 @@
-def run():
-    print('Hi')
+import click
+
+
+@click.command()
+@click.option('-v', '--verbose', is_flag=True, default=False)
+@click.option('--images/--no-images', ' /-I', default=True)
+@click.option('-c', '--chrome', 'driver', flag_value='chrome')
+@click.option('-g', '--gecko', 'driver', flag_value='gecko', default=True)
+@click.argument('url')
+def run(url, images, driver, verbose):
+    if verbose:
+        print(f"""Scrapping:    {url}
+    driver:   {driver}
+    images:   {images}
+    verbose:  {verbose}
+""")
 
 
 if __name__ == '__main__':
